dc452e973ec856b5ff551df946a4cabfb5d586df
Shubham Kumar Maurya <yesitsskm@gmail.com>
2022-03-30 20:24:11 +0100

    Scene updates.

--- VR_GroupProject/Assets/Shubham/Scripts/FeedingSystem.cs
+++ VR_GroupProject/Assets/Shubham/Scripts/FeedingSystem.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class FeedingSystem : MonoBehaviour
 {
     public SpiderStateManager spiderState;
+
+   // public XRBaseController xRController;
 
     void OnTriggerEnter(Collider collision)
     {
@@ -11,6 +14,8 @@ public class FeedingSystem : MonoBehaviour
             Destroy(collision.transform.gameObject, 0.1f);
 
             spiderState.SwitchStates(spiderState.happyState);
+
+            //xRController.SendHapticImpulse(0.7f, 2f);
         }
 
         else if (spiderState.currentStateNumber == 3 && (collision.transform.CompareTag("Pills")))
@@ -18,6 +23,8 @@ public class FeedingSystem : MonoBehaviour
             Destroy(collision.transform.gameObject, 0.1f);
 
             spiderState.SwitchStates(spiderState.happyState);
+
+            //xRController.SendHapticImpulse(0.7f, 2f);
         }
     }
 }
